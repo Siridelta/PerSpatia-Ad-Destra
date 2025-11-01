7ad5f5686c2528100be26f6b45aec21e01e6f305
Static1248 <static1248@qq.com>
2025-11-01 20:22:42 +0800

    fix: modified "export desmos preview" icon

--- solution-react-flow/src/components/TextNode/displays/OutputDisplay.tsx
+++ solution-react-flow/src/components/TextNode/displays/OutputDisplay.tsx
@@ -5,7 +5,7 @@ interface ExportableOutputInfo {
   name: string;
   value: unknown;
   isExportable: boolean;
-  reason?: string;
+  // reason?: string;
 }
 
 export interface OutputDisplayProps {
@@ -28,7 +28,7 @@ const OutputDisplay: React.FC<OutputDisplayProps> = ({ outputs, isAnimatingOut =
         name,
         value,
         isExportable: isObject || isArrayOfObjects,
-        reason: isObject || isArrayOfObjects ? undefined : '仅支持对象格式输出',
+        // reason: isObject || isArrayOfObjects ? undefined : '仅支持对象格式输出',
       };
     });
   }, [outputs]);
@@ -52,22 +52,17 @@ const OutputDisplay: React.FC<OutputDisplayProps> = ({ outputs, isAnimatingOut =
   };
 
   const renderExportButton = (info: ExportableOutputInfo) => {
-    const disabled = !info.isExportable;
-
     return (
       <button
         className="output-export-button"
         type="button"
         onClick={() => handleExport(info)}
-        disabled={disabled}
-        title={disabled && info.reason ? info.reason : '导出到 Desmos 预览'}
-        aria-label={`导出 ${info.name} 到 Desmos 预览`}
+        title={'导出 Desmos 预览'}
+        aria-label={`导出 ${info.name} 的 Desmos 预览`}
       >
-        <svg viewBox="0 0 24 24" width="14" height="14" aria-hidden="true" focusable="false">
-          <path
-            fill="currentColor"
-            d="M5 19h4v2H5a2 2 0 0 1-2-2v-4h2v4zm11-16h-4V1h4a2 2 0 0 1 2 2v4h-2V3zm-9.95 2.05 2.828-2.828 1.414 1.414-2.829 2.828zM19 15h2v4a2 2 0 0 1-2 2h-4v-2h4zm-7-7 3 3-1.414 1.414L13 11.828V20h-2v-8.172l-1.586 1.586L8 11l3-3z"
-          />
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9.33398 6.66699C9.70175 6.66743 9.99991 6.96516 10 7.33301C10 7.70093 9.7018 7.99858 9.33398 7.99902V8.00098H8.00098V16.001H16.001V15.0088C16.0009 15.0059 16 15.0029 16 15C16.0001 14.6319 16.2989 14.334 16.667 14.334C17.035 14.3342 17.3329 14.632 17.333 15L17.334 16.001C17.3335 16.7367 16.7367 17.3335 16.001 17.334H8.00098C7.26487 17.334 6.66646 16.737 6.66602 16.001V8.00098C6.66602 7.2646 7.2646 6.66602 8.00098 6.66602L9.33398 6.66699Z" fill="white" />
+          <path d="M17 5.99902C17.3682 5.99902 17.667 6.2988 17.667 6.66699V12C17.6667 12.368 17.368 12.667 17 12.667C16.632 12.667 16.3333 12.368 16.333 12V8.35254L12.4863 12.4561C12.2346 12.7243 11.8125 12.7378 11.5439 12.4863C11.2756 12.2346 11.2622 11.8125 11.5137 11.5439L15.4609 7.33398H12C11.632 7.33398 11.3333 7.03496 11.333 6.66699C11.333 6.2988 11.6318 5.99902 12 5.99902H17Z" fill="white" />
         </svg>
       </button>
     );
@@ -97,7 +92,7 @@ const OutputDisplay: React.FC<OutputDisplayProps> = ({ outputs, isAnimatingOut =
         <span className="output-variable-name">{name}</span>
         <span className="output-variable-type">:{type}</span>
         <span className="output-variable-value">{valueStr}</span>
-        {renderExportButton(info)}
+        {info.isExportable && renderExportButton(info)}
       </div>
     );
   };
