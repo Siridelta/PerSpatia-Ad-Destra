a58d47b97c25d4140cd9305edaf86bcfbc9505eb
Static1248 <static1248@qq.com>
2026-04-12 01:01:15 +0800

    refactor: clean up forgetme

--- r3f-reactflow-nextui/forgetme/components/TextNode/controls/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as SliderControl } from './SliderControl';
-export { default as ToggleControl } from './ToggleControl';
-export { default as TextControl } from './TextControl';
-
-export type { SliderControlProps } from './SliderControl';
-export type { ToggleControlProps } from './ToggleControl';
-export type { TextControlProps } from './TextControl'; 
--- r3f-reactflow-nextui/forgetme/components/TextNode/displays/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as ErrorDisplay } from './ErrorDisplay';
-export { default as WarningDisplay } from './WarningDisplay';
-export { default as LogDisplay } from './LogDisplay';
-export { default as OutputDisplay } from './OutputDisplay';
-
-export type { ErrorDisplayProps, ErrorInfo } from './ErrorDisplay';
-export type { WarningDisplayProps, WarningInfo } from './WarningDisplay';
-export type { LogDisplayProps } from './LogDisplay';
-export type { OutputDisplayProps } from './OutputDisplay'; 
